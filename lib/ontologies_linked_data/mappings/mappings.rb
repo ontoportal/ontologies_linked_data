3a73b1fdf9db52ccaaf1123907dfa5afbbbe0d7e
Vincent Emonet <vincent.emonet@gmail.com>
2015-07-17 10:44:41 +0200

    now use get_mapping_classes in get_rest_mapping to handle interportal and external classes

--- lib/ontologies_linked_data/mappings/mappings.rb
+++ lib/ontologies_linked_data/mappings/mappings.rb
@@ -432,7 +432,7 @@ eos
   end
 
   def self.get_rest_mapping(mapping_id)
-    backup = LinkedData::Models::RestBackupMapping.find(mapping_id).first
+    backup = LinkedData::Models::RestBackupMapping.find(mapping_id).include(:class_urns).first
     if backup.nil?
       return nil
     end
@@ -457,8 +457,7 @@ eos
     mapping = nil
     epr.query(qmappings,
               graphs: graphs).each do |sol|
-      classes = [ read_only_class(sol[:c1].to_s,sol[:s1].to_s),
-                read_only_class(sol[:c2].to_s,sol[:s2].to_s) ]
+      classes = get_mapping_classes(sol[:c1].to_s, sol[:s1].to_s, sol[:c2].to_s, sol[:s2].to_s, backup)
       process = LinkedData::Models::MappingProcess.find(sol[:o]).first
       mapping = LinkedData::Models::Mapping.new(classes,"REST",
                                                 process,
