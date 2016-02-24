bfbab1cfbaa60e4174eae6e53e7f7f97d74f8ea2
Vincent Emonet <vincent.emonet@lirmm.fr>
2016-02-24 16:33:10 +0100

    create a function to send a mail to the admin when a new user is created

--- lib/ontologies_linked_data/config/config.rb
+++ lib/ontologies_linked_data/config/config.rb
@@ -72,6 +72,8 @@ module LinkedData
     @settings.smtp_auth_type                ||= :none # :none, :plain, :login, :cram_md5
     @settings.smtp_domain                   ||= "localhost.localhost"
     @settings.enable_starttls_auto          ||= false # set to true for use with gmail
+    # email of the instance administrator to get mail notifications when new user
+    @settings.email_admin                   ||= "admin@example.org"
 
     # Override defaults
     yield @settings, overide_connect_goo if block_given?
