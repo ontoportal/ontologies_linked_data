beecf66c1a43eaac5754678bef923803d88a85d4
Vincent Emonet <vincent.emonet@gmail.com>
2016-06-02 14:35:31 +0200

    comment the uri attribute for tests

--- lib/ontologies_linked_data/models/ontology_submission.rb
+++ lib/ontologies_linked_data/models/ontology_submission.rb
@@ -37,7 +37,7 @@ module LinkedData
       #attribute :publication, enforce: [:list], extractedMetadata: true, metadataMappings: ["omv:reference", "dct:bibliographicCitation", "foaf:isPrimaryTopicOf", "schema:citation", "cito:citesAsAuthority", "schema:citation"] # TODO: change default attribute name
       attribute :publication, extractedMetadata: true, metadataMappings: ["omv:reference", "dct:bibliographicCitation", "foaf:isPrimaryTopicOf", "schema:citation", "cito:citesAsAuthority", "schema:citation"] # TODO: change default attribute name
 
-      attribute :URI, namespace: :omv # attention, attribute particulier. Je le récupère proprement via OWLAPI
+      #attribute :uri, namespace: :omv # attention, attribute particulier. Je le récupère proprement via OWLAPI
       attribute :naturalLanguage, namespace: :omv, enforce: [:list], extractedMetadata: true, metadataMappings: ["dc:language", "dct:language", "doap:language", "schema:inLanguage"]
       #attribute :documentation, namespace: :omv, enforce: [:list], extractedMetadata: true, metadataMappings: ["rdfs:seeAlso", "foaf:page", "vann:usageNote", "mod:document", "dcat:landingPage", "doap:wiki"]
       attribute :documentation, namespace: :omv, extractedMetadata: true, metadataMappings: ["rdfs:seeAlso", "foaf:page", "vann:usageNote", "mod:document", "dcat:landingPage", "doap:wiki"]
@@ -720,7 +720,7 @@ module LinkedData
         # Automaticaly generate some metadata
 
         # Retrieve ontology URI attribute directly with OWLAPI
-        self.URI = ontology_uri
+        #self.URI = ontology_uri
         # Metadata specific to BioPortal that have been removed:
         #if self.hostedBy.nil?
         #  self.hostedBy = [ RDF::URI.new("http://#{LinkedData.settings.ui_host}") ]
