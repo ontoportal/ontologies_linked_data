4ce585771d99b15c51e6234b8303e26d10a05f25
Vincent Emonet <vincent.emonet@gmail.com>
2017-05-05 17:26:35 -0700

    In new note notification now test for [bioportal.bioontology.org Notes] instead of [BioPortal Notes]; because we are using ui_host params to generate the email subject

--- test/util/test_notifications.rb
+++ test/util/test_notifications.rb
@@ -72,7 +72,8 @@ class TestNotifications < LinkedData::TestCase
       note.relatedOntology = [@@ont]
       note.save
 
-      assert last_email_sent.subject.include?("[BioPortal Notes]")
+      assert last_email_sent.subject.include?("[bioportal.bioontology.org Notes]"),
+             "#{last_email_sent.subject} instead of [bioportal.bioontology.org Notes]. Make sure params ui_host is not set in the config.rb file (or set on bioportal.bioontology.org)"
       assert_equal [@@user.email], last_email_sent.to
     ensure
       note.delete if note
