c18270dcb4d773b274b93db7611e4358e9ebf2ed
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-04-04 14:54:21 +0200

    configurize the hardcoded Bioportal name

--- lib/ontologies_linked_data/utils/notifications.rb
+++ lib/ontologies_linked_data/utils/notifications.rb
@@ -43,10 +43,11 @@ module LinkedData::Utils
       subject = "[#{LinkedData.settings.ui_host} Notes] [#{ontologies}] #{note.subject}"
       body = NEW_NOTE.gsub("%username%", note.creator.username)
                      .gsub("%ontologies%", ontologies)
-                     .gsub("%note_url%", LinkedData::Hypermedia.generate_links(note)["ui"])
                      .gsub("%note_subject%", note.subject || "")
                      .gsub("%note_body%", note.body || "")
       
+                     .gsub("%note_url%", LinkedData::Hypermedia.generate_links(note)["ui"])
+                     .gsub('%ui_name%', LinkedData.settings.ui_name)
 
       options = {
         ontologies: note.relatedOntology,
@@ -70,6 +71,7 @@ module LinkedData::Utils
                                  .gsub('%statuses%', status.join('<br/>'))
                                  .gsub('%admin_email%', LinkedData.settings.email_sender)
                                  .gsub('%ontology_location%', LinkedData::Hypermedia.generate_links(ontology)['ui'])
+                                 .gsub('%ui_name%', LinkedData.settings.ui_name)
 
       options = {
         ontologies: ontology,
@@ -90,6 +92,8 @@ module LinkedData::Utils
                                 .gsub('%ont_name%', ontology.name)
                                 .gsub('%ont_acronym%', ontology.acronym)
                                 .gsub('%ontology_location%', LinkedData::Hypermedia.generate_links(ontology)['ui'])
+                                .gsub('%support_mail%', support_mails.first)
+                                .gsub('%ui_name%', LinkedData.settings.ui_name)
       recipients = []
       ontology.administeredBy.each do |user|
         user.bring(:email) if user.bring?(:email)
@@ -115,6 +119,7 @@ module LinkedData::Utils
       body = NEW_USER_CREATED.gsub('%username%', user.username.to_s)
                              .gsub('%email%', user.email.to_s)
                              .gsub('%site_url%', LinkedData.settings.ui_host)
+                             .gsub('%ui_name%', LinkedData.settings.ui_name)
       recipients = LinkedData.settings.admin_emails
 
       options = {
@@ -134,6 +139,7 @@ module LinkedData::Utils
                                  .gsub('%addedby%', ont.administeredBy[0].to_s)
                                  .gsub('%site_url%', LinkedData.settings.ui_host)
                                  .gsub('%ont_url%', LinkedData::Hypermedia.generate_links(ont)['ui'])
+                                 .gsub('%ui_name%', LinkedData.settings.ui_name)
       recipients = LinkedData.settings.admin_emails
 
       options = {
@@ -145,9 +151,10 @@ module LinkedData::Utils
     end
 
     def self.reset_password(user, token)
-      subject = "[BioPortal] User #{user.username} password reset"
+      ui_name = LinkedData.settings.ui_name
+      subject = "[#{ui_name}] User #{user.username} password reset"
       password_url = "https://#{LinkedData.settings.ui_host}/reset_password?tk=#{token}&em=#{CGI.escape(user.email)}&un=#{CGI.escape(user.username)}"
-      
+
       body = <<~HTML
         Someone has requested a password reset for user #{user.username}. If this was 
         you, please click on the link below to reset your password. Otherwise, please 
@@ -157,6 +164,7 @@ module LinkedData::Utils
 
         Thanks,<br/>
         BioPortal Team
+        #{ui_name} Team
       HTML
 
       options = {
@@ -169,9 +177,9 @@ module LinkedData::Utils
 
     def self.obofoundry_sync(missing_onts, obsolete_onts)
       body = ''
-
+      ui_name = LinkedData.settings.ui_name
       if missing_onts.size > 0
-        body << '<strong>The following OBO Library ontologies are missing from BioPortal:</strong><br/><br/>'
+        body << "<strong>The following OBO Library ontologies are missing from #{ui_name}:</strong><br/><br/>"
         missing_onts.each do |ont|
           body << "<a href='#{ont['homepage']}'>#{ont['id']}</a> (#{ont['title']})<br/><br/>"
         end
@@ -185,7 +193,7 @@ module LinkedData::Utils
       end
 
       if body.empty?
-        body << 'BioPortal and the OBO Foundry are in sync.<br/><br/>'
+        body << "#{ui_name} and the OBO Foundry are in sync.<br/><br/>"
       end
 
       options = {
@@ -252,35 +260,37 @@ A new note was added to %ontologies% by <b>%username%</b>.<br/><br/>
 %note_body%<br/>
 ----------------------------------------------------------------------------------<br/><br/>
 
-You can respond by visiting: <a href="%note_url%">NCBO BioPortal</a>.<br/><br/>
+You can respond by visiting: <a href="%note_url%">%ui_name%</a>.<br/><br/>
 EOS
 
-SUBMISSION_PROCESSED = <<EOS
-%ontology_name% (%ontology_acronym%) was processed for use in BioPortal. Here are the results:
+    SUBMISSION_PROCESSED = <<EOS
+%ontology_name% (%ontology_acronym%) was processed for use in %ui_name%. Here are the results:
 <br><br>
 %statuses%
 <br><br>
 Please contact %admin_email% if you have questions.
 <br><br>
-The ontology can be <a href="%ontology_location%">browsed in BioPortal</a>.
+The ontology can be <a href="%ontology_location%">browsed in %ui_name%</a>.
 <br><br>
 Thank you,<br>
-The BioPortal Team
+The %ui_name% Team
 EOS
 
-REMOTE_PULL_FAILURE = <<EOS
-BioPortal failed to load %ont_name% (%ont_acronym%) from URL: %ont_pull_location%.
+    REMOTE_PULL_FAILURE = <<EOS
+%ui_name% failed to load %ont_name% (%ont_acronym%) from URL: %ont_pull_location%.
 <br><br>
 Please verify the URL you provided for daily loading of your ontology:
 <ol>
-<li>Make sure you are signed in to BioPortal.</li>
-<li>Navigate to your ontology summary page: %ontology_location%.</li>
+<li>Make sure you are signed in to %ui_name%.</li>
+<li>Navigate to your ontology summary page: <a href="%ontology_location%">%ontology_location%</a>.</li>
 <li>Click the &quot;Edit submission information&quot; link.</li>
 <li>In the Location row, verify that you entered a valid URL for daily loading of your ontology in the URL text area.</li>
 </ol>
-If you need further assistance, please <a href="mailto:support@bioontology.org">contact us</a> via the BioPortal support mailing list.
+If you need further assistance, please <a href="mailto:%support_mail%">contact us</a> via the %ui_name% support mailing list.
 <br><br>
 Thank you,<br>
+The %ui_name% Team
+EOS
 
     NEW_USER_CREATED = <<EOS
 A new user have been created on %site_url%
@@ -289,7 +299,7 @@ Username: %username%
 <br>
 Email: %email%
 <br><br>
-The BioPortal Team
+The %ui_name% Team
 EOS
 
     NEW_ONTOLOGY_CREATED = <<EOS
@@ -301,7 +311,7 @@ Name: %name%
 <br>
 At <a href="%ont_url%">%ont_url%</a>
 <br><br>
-The BioPortal Team
+The %ui_name% Team
 EOS
 
   end
