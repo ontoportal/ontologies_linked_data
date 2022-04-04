84ac55b2e032dbf3a3ff9fa047758a017e8ab2a3
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-04-04 12:42:24 +0200

    add submission process operation abstraction

--- lib/ontologies_linked_data.rb
+++ lib/ontologies_linked_data.rb
@@ -34,6 +34,7 @@ require "ontologies_linked_data/models/base"
 
 # Require all models
 project_root = File.dirname(File.absolute_path(__FILE__))
+require 'ontologies_linked_data/services/submission_process/submission_process'
 
 # We need to require deterministic - that is why we have the sort.
 models = Dir.glob(project_root + '/ontologies_linked_data/models/**/*.rb').sort
