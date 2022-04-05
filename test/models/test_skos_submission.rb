03ed83e5697af63287748c93738a9b036c7e2fc1
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-04-06 01:19:48 +0200

    add skos submission tests

--- test/models/test_skos_submission.rb
+++ test/models/test_skos_submission.rb
@@ -1,25 +1,53 @@
-require_relative "./test_ontology_common"
-require "logger"
-require "rack"
+require_relative './test_ontology_common'
+require 'logger'
+require 'rack'
 
 class TestOntologySubmission < LinkedData::TestOntologyCommon
-  def test_get_main_concept_scheme
-    submission_parse("INRAETHES",
-                     "INRAETHES",
-                     "./test/data/ontology_files/thesaurusINRAE_nouv_structure.skos.rdf", 1,
+
+  def test_skos_ontology
+    submission_parse('SKOS-TEST',
+                     'SKOS TEST Bla',
+                     './test/data/ontology_files/efo_gwas.skos.owl', 987,
                      process_rdf: true, index_search: false,
                      run_metrics: false, reasoning: true)
-    sub = LinkedData::Models::OntologySubmission.where(ontology: [acronym: "INRAETHES"],
-                                                       submissionId: 1)
+
+    sub = LinkedData::Models::OntologySubmission.where(ontology: [acronym: 'SKOS-TEST'],
+                                                       submissionId: 987)
                                                 .include(:version)
                                                 .first
-    sub.bring_remaining
-    sub.URI = "http://opendata.inrae.fr/thesaurusINRAE/thesaurusINRAE"
-    sub.save
-    #schemas = LinkedData::Models::Instance.where({types: RDF::URI.new(RDF::SKOS[:ConceptScheme])}).in(sub).all
+
+    assert sub.roots.map { |x| x.id.to_s }.sort == ['http://www.ebi.ac.uk/efo/EFO_0000311',
+                                                    'http://www.ebi.ac.uk/efo/EFO_0001444',
+                                                    'http://www.ifomis.org/bfo/1.1/snap#Disposition',
+                                                    'http://www.ebi.ac.uk/chebi/searchId.do?chebiId=CHEBI:37577',
+                                                    'http://www.ebi.ac.uk/efo/EFO_0000635',
+                                                    'http://www.ebi.ac.uk/efo/EFO_0000324'].sort
     roots = sub.roots
-    assert_equal  12, roots.size
-    assert !sub.nil?
+    LinkedData::Models::Class.in(sub).models(roots).include(:children).all
+    roots.each do |root|
+      q_broader = <<-eos
+SELECT ?children WHERE {
+  ?children #{RDF::SKOS[:broader].to_ntriples} #{root.id.to_ntriples} }
+      eos
+      children_query = []
+      Goo.sparql_query_client.query(q_broader).each_solution do |sol|
+        children_query << sol[:children].to_s
+      end
+      assert root.children.map { |x| x.id.to_s }.sort == children_query.sort
+    end
+  end
+
+  def test_get_main_concept_scheme
+    submission_parse('SKOS-TEST',
+                     'SKOS TEST Bla',
+                     './test/data/ontology_files/efo_gwas.skos.owl', 987,
+                     process_rdf: true, index_search: false,
+                     run_metrics: false, reasoning: true)
+    sub = LinkedData::Models::OntologySubmission.where(ontology: [acronym: 'SKOS-TEST'],
+                                                       submissionId: 987)
+                                                .first
+
+    assert_equal 'http://www.ebi.ac.uk/efo/skos/EFO_GWAS_view', sub.get_main_concept_scheme.to_s
   end
 end
 
