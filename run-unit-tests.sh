ecefccb70a3f876be64aaf26fbe6a31b896fab45
Alex Skrenchuk <alexskr@stanford.edu>
2022-05-27 11:55:15 -0700

    add github action for running unit tests with AllegroGraph triple store

--- run-unit-tests.sh
+++ run-unit-tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # sample script to run unit tests with docker
 
+#DC='docker-compose --profile 4store'
+DC='docker-compose'
+
 # unit test expects config file even though all settings are set via env vars.
 [ -f config/config.rb ] || cp config/config.rb.sample config/config.rb
 
@@ -8,10 +11,8 @@
 test/solr/generate_ncbo_configsets.sh
 
 # build docker containers
-docker-compose build
+$DC up --build -d
+#$DC run --rm ruby wait-for-it solr-ut:8983 -- bundle exec rake test TESTOPTS='-v' TEST='./test/models/test_mappings.rb'
+$DC run --rm ruby wait-for-it solr-ut:8983 -- bundle exec rake test TESTOPTS='-v'
 
-#docker-compose up --exit-code-from test
-#docker-compose run --rm test wait-for-it solr-ut:8983 -- bundle exec rake test TESTOPTS='-v'
-docker-compose run --rm test wait-for-it solr-ut:8983 -- bundle exec rake test
-
-docker-compose down
+$DC down
