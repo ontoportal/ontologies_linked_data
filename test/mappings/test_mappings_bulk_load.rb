b3a4d9274b1f9303b65142c241c34b6ec78d0c22
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-08-19 15:54:22 +0200

    fix mappings tests by deleting them after test

--- test/mappings/test_mappings_bulk_load.rb
+++ test/mappings/test_mappings_bulk_load.rb
@@ -93,42 +93,34 @@ class TestMappingBulkLoad < LinkedData::TestOntologyCommon
   end
 
   private
-
-  def commun_test(mapping_hash, ontology_id)
-    mappings, rest_mapping_count = mapping_load(mapping_hash, ontology_id)
-    mappings.each do |m|
-      next unless m.source == 'REST'
-
-      rest_mapping_count += 1
-      assert_equal m.classes.length, 2
-      c1 =  m.classes.select {
-        |c| c.id.to_s['Image_Algorithm'] }.first
-      c2 = m.classes.select {
-        |c| c.id.to_s['cno_0000202'] }.first
-      assert !c1.nil?
-      assert !c2.nil?
-
-      assert_equal m.process.relation.to_s,
-                   'http://www.w3.org/2002/07/owl#subClassOf'
-
-      assert_equal m.process.subject_source_id.to_s,
-                   'http://bioontology.org/ontologies/BiomedicalResources.owl'
-
-      assert_equal m.process.object_source_id.to_s,
-                   'http://purl.org/incf/ontology/Computational_Neurosciences/cno_alpha.owl'
-
-    end
-    assert_equal rest_mapping_count, 1
-    delete_rest_mappings
-  end
-
   def delete_rest_mappings
     LinkedData::Models::RestBackupMapping.all.each do |m|
       LinkedData::Mappings.delete_rest_mapping(m.id)
     end
   end
 
+  def commun_test(mapping_hash, ontology_id)
+    mappings = mapping_load(mapping_hash, ontology_id)
+    selected = mappings.select do |m|
+      m.source == 'REST' &&
+        m.classes.first.id.to_s['Image_Algorithm'] &&
+        m.classes.last.id.to_s['cno_0000202']
+    end
+    selected = selected.first
+    refute_nil selected
+    assert_equal Array(selected.process.relation),
+                 ['http://www.w3.org/2002/07/owl#subClassOf']
+
+    assert_equal selected.process.subject_source_id.to_s,
+                 'http://bioontology.org/ontologies/BiomedicalResources.owl'
+
+    assert_equal selected.process.object_source_id.to_s,
+                 'http://purl.org/incf/ontology/Computational_Neurosciences/cno_alpha.owl'
+
+  end
+
   def mapping_load(mapping_hash, ontology_id)
+    delete_rest_mappings
     user_name = 'test_mappings_user'
     user = LinkedData::Models::User.where(username: user_name).include(:username).first
     if user.nil?
@@ -146,8 +138,8 @@ class TestMappingBulkLoad < LinkedData::TestOntologyCommon
                                     .first
     latest_sub = o.nil? ? nil : o.latest_submission
     mappings = LinkedData::Mappings.mappings_ontology(latest_sub, 1, 1000)
-    rest_mapping_count = 0
-    [mappings, rest_mapping_count]
+
+    mappings
   end
 end
 
