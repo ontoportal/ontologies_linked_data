218c17ef833774696a156fb733abf048f2031738
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-03-16 04:23:30 +0100

    add status_align_callbacks tests

--- test/models/test_ontology_submission_validators.rb
+++ test/models/test_ontology_submission_validators.rb
@@ -5,21 +5,10 @@ require "rack"
 class TestOntologySubmissionValidators < LinkedData::TestOntologyCommon
 
   def test_enforce_symmetric_ontologies
-    ontologies = []
-    3.times do |i|
-      submission_parse("TEST-ONT-#{i}",
-                       "TEST ONT  #{i}",
-                       './test/data/ontology_files/fake_for_mappings.owl', i,
-                       process_rdf: true, index_search: false,
-                       run_metrics: false, reasoning: false)
-    end
+    ont_count, ont_acronyms, ontologies =
+      create_ontologies_and_submissions(ont_count: 3, submission_count: 1,
+                                        process_submission: false, acronym: 'NCBO-545')
 
-    3.times do |i|
-      ont = LinkedData::Models::Ontology.where(acronym: "TEST-ONT-#{i}").first
-      next unless ont
-
-      ontologies << ont
-    end
 
     assert_equal 3, ontologies.size
 
@@ -53,5 +42,41 @@ class TestOntologySubmissionValidators < LinkedData::TestOntologyCommon
     first_sub.bring :ontologyRelatedTo
     assert_equal [ontologies[1].id], first_sub.ontologyRelatedTo
 
+  end
+
+  def test_status_align_callbacks
+    ont_count, ont_acronyms, ontologies =
+      create_ontologies_and_submissions(ont_count: 1, submission_count: 3,
+                                        process_submission: false, acronym: 'NCBO-545')
+
+
+
+
+    # Sanity check.
+    assert_equal 1, ontologies.count
+    ont = ontologies.first
+    ont.bring :submissions
+    ont.submissions.each { |s| s.bring(:submissionId) }
+    assert_equal 3, ont.submissions.count
+
+    # Sort submissions in descending order.
+    sorted_submissions = ont.submissions.sort { |a,b| b.submissionId <=> a.submissionId }
+
+    latest = sorted_submissions.first
+
+    latest.bring :status, :deprecated
+
+    assert_equal 'alpha', latest.status
+    assert_equal false, latest.deprecated
+
+    latest.status = 'retired'
+    latest.bring_remaining
+    latest.save
+
+    sorted_submissions.each do |s|
+      s.bring :status, :deprecated
+      assert_equal 'retired', latest.status
+      assert_equal true, latest.deprecated
+    end
   end
 end
