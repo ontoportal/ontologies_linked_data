8159914db1c290de8aed460e1c53fbff5395657a
Alex Skrenchuk <alexskr@stanford.edu>
2023-07-27 09:38:43 -0700

    update Gemfile.lock

--- run-unit-tests.sh
+++ run-unit-tests.sh
@@ -2,18 +2,16 @@
 # sample script to run unit tests with docker
 
 #DC='docker-compose --profile 4store'
-DC='docker-compose'
+DC='docker compose'
 
 # unit test expects config file even though all settings are set via env vars.
-[ -f config/config.rb ] || cp config/config.rb.sample config/config.rb
+[ -f config/config.rb ] || cp config/config.test.rb config/config.rb
 
 # generate solr configsets for solr container
 test/solr/generate_ncbo_configsets.sh
 
 # build docker containers
-#$DC up --build -d
-#$DC run --rm ruby wait-for-it solr-ut:8983 -- bundle exec rake test TESTOPTS='-v' TEST='./test/models/test_mappings.rb'
 $DC run --rm ruby bundle exec rake test TESTOPTS='-v'
 # run unit test with AG backend
 #$DC run --rm ruby-agraph bundle exec rake test TESTOPTS='-v'
-#$DC down
+$DC stop
