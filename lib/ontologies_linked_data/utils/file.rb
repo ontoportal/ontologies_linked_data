ce4d2c4b4c40c1643ecdff7a9bd54cf1ac703cb8
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-09-09 17:21:04 +0200

    use "zlib" to read and unzip Gz files and raise an error  if unsupported file  submitted

--- lib/ontologies_linked_data/utils/file.rb
+++ lib/ontologies_linked_data/utils/file.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'uri'
 require 'zip'
+require 'zlib'
 require 'tmpdir'
 
 module LinkedData
@@ -20,7 +21,7 @@ module LinkedData
         raise ArgumentError, "File path #{file_path} not found" unless File.exist? file_path
 
         file_type = `file --mime -b #{Shellwords.escape(file_path)}`
-        file_type.split(';')[0] == 'application/gzip'
+        file_type.split(';')[0] == 'application/gzip' || file_type.split(';')[0] == 'application/x-gzip'
       end
 
       def self.files_from_zip(file_path)
@@ -30,15 +31,22 @@ module LinkedData
         end
 
         files = []
-        Zip::File.open(file_path) do |zipfile|
-          zipfile.each do |file|
-            if not file.directory?
-              if not file.name.split('/')[-1].start_with? '.' #a hidden file in __MACOSX or .DS_Store
+        if gzip?(file_path)
+          Zlib::GzipReader.open(file_path) do |file|
+            files << file.orig_name unless File.directory?(file) || file.orig_name.split('/')[-1].start_with?('.') # a hidden file in __MACOSX or .DS_Store
+          end
+        elsif zip?(file_path)
+          Zip::File.open(file_path) do |zip_files|
+            zip_files.each do |file|
+              unless file.directory? || file.name.split('/')[-1].start_with?('.') # a hidden file in __MACOSX or .DS_Store
                 files << file.name
               end
             end
           end
+        else
+          raise StandardError, "Unsupported file format: #{File.extname(file_path)}"
         end
+
         return files
       end
 
@@ -52,21 +60,20 @@ module LinkedData
         if gzip?(file_path)
           Zlib::GzipReader.open(file_path) do |gz|
             File.open([dst_folder, gz.orig_name].join('/'), "w") { |file| file.puts(gz.read) }
-            extracted_files << GzipFile.new(gz)
+            extracted_files << gz
           end
-        else
+        elsif zip?(file_path)
           Zip::File.open(file_path) do |zipfile|
             zipfile.each do |file|
               if file.name.split('/').length > 1
-                sub_folder = File.join(dst_folder,
-                                      file.name.split('/')[0..-2].join('/'))
-                unless Dir.exist?(sub_folder)
-                  FileUtils.mkdir_p sub_folder
-                end
+                sub_folder = File.join(dst_folder, file.name.split('/')[0..-2].join('/'))
+                FileUtils.mkdir_p sub_folder unless Dir.exist?(sub_folder)
               end
-              extracted_files << file.extract(File.join(dst_folder,file.name))
+              extracted_files << file.extract(File.join(dst_folder, file.name))
             end
           end
+        else
+          raise StandardError, "Unsupported file format: #{File.extname(file_path)}"
         end
         extracted_files
       end
