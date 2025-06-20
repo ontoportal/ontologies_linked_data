fe963da4872b89d68a0871d5d1686ff30e14646d
mdorf <mdorf@stanford.edu>
2025-06-19 21:26:06 -0700

    fixed an error that was introduced by #158 and returned after the merge

--- lib/ontologies_linked_data/mappings/mappings.rb
+++ lib/ontologies_linked_data/mappings/mappings.rb
@@ -320,7 +320,7 @@ FILTER(?s1 != ?s2)
         classes = [read_only_class(sol[:c1].to_s, sol[:s1].to_s),
                    read_only_class(sol[:c2].to_s, sol[:s2].to_s)]
         process = LinkedData::Models::MappingProcess.find(sol[:o]).first
-        process.bring_remaining unless process.nil?
+        # process.bring_remaining unless process.nil?
         mapping = LinkedData::Models::Mapping.new(classes, 'REST',
                                                   process,
                                                   sol[:uuid])
